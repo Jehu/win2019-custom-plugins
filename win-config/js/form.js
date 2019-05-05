a6974956b0d93be7c2cde1a6997f84b0f1bb9399
Andreas Behr <andreas@andreas-behr.de>
2019-05-05 16:05:12 -0400

    contact form 7 -> inxmail integration

---
+++ win-config/js/form.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function() {
+
+    let wpcf7Elm = document.querySelector( '.wpcf7' );
+
+    wpcf7Elm.addEventListener( 'wpcf7submit', function( event ) {
+        let action = jQuery("div#" + event.detail.id).find("form").attr("action");
+        let $form = jQuery("<form id=\"dc_form\" action=\"" + action + "\" method=\"POST\"></form>");
+        jQuery(event.detail.inputs).each(function(k,v) {
+            $form.append("<input type=\"hidden\" name=\"" + v["name"] + "\" value=\"" + v["value"] + "\"/>");
+        });
+        jQuery(document.body).append($form);
+        $form.submit();
+    },false);
+});
--- win-config/win-config.php
+++ win-config/win-config.php
@@ -26,29 +26,56 @@ function win_config()
     //var_dump(get_option('win_config_options')['white_paper_formular_id']);
     ?>
     <div class="wrap">
+        <style>
+            fieldset {
+                border:2px solid #FFF;
+                padding:5px;
+                margin-bottom: 15px;
+            }
+            fieldset legend {
+                font-weight: bold;
+            }
+            input[type=text] {
+                width: 300px;
+            }
+            label {
+                display: block;
+            }
+        </style>
         <h1>Einstellungen</h1>
-        <div style="border:2px solid #FFF; padding:5px;" >
-
-            <form method="post" action="options.php">
+        <form method="post" action="options.php">
+            <fieldset>
                 <?php
                 settings_fields('einstellungen_gruppe');
                 $config = get_option('win_config_options');
                 ?>
 
-                <label>Whitepaper Lead-Formular-ID: </label><br/>
-                <input style="width:400px;" type="text" name="win_config_options[white_paper_formular_id]"
+                <label>Whitepaper Lead-Formular-ID: </label>
+                <input type="text" name="win_config_options[white_paper_formular_id]"
                        value="<?php echo (isset($config['white_paper_formular_id'])) ? $config['white_paper_formular_id'] : ""; ?>" />
                 <br/>
 
-                <label>JobWare Url: </label><br/>
-                <input style="width:400px;" type="text" name="win_config_options[jobware_url]"
+                <label>JobWare Url: </label>
+                <input type="text" name="win_config_options[jobware_url]"
                        value="<?php echo (isset($config['jobware_url'])) ? $config['jobware_url'] : ""; ?>" />
                 <br/>
 
-                <p><input type="submit" class="button-primary" value="<?php _e('Save') ?>"/></p>
 
-            </form>
-        </div>
+            </fieldset>
+            <fieldset>
+                <legend><?php echo __('Inxmail Settings') ?></legend>
+
+                <label><?php echo __('Login URL') ?></label>
+                <input type="text" name="win_config_options[inxmail_login_url]"
+                value="<?php echo (isset($config['inxmail_login_url'])) ? $config['inxmail_login_url'] : '' ?>" />
+
+                <label><?php echo __('Submit URL') ?></label>
+                <input type="text" name="win_config_options[inxmail_submit_url]"
+                       value="<?php echo (isset($config['inxmail_submit_url'])) ? $config['inxmail_submit_url'] : '' ?>" />
+
+            </fieldset>
+            <p><input type="submit" class="button-primary" value="<?php _e('Save') ?>"/></p>
+        </form>
     </div>
 <?php
 }
@@ -66,3 +93,28 @@ function win_config_validate($input)
 
 add_action('admin_init', 'win_config_init');
 
+
+// change action url of contact form
+
+add_filter('wpcf7_form_action_url', 'wpcf7_custom_form_action_url');
+
+function wpcf7_custom_form_action_url($url)
+{
+    global $post;
+
+    if ($post->post_name === 'redaktionsbrief') {
+
+        return get_option('win_config_options')['inxmail_submit_url'];
+
+    } else {
+
+        return $url;
+    }
+}
+
+add_action('wp_enqueue_scripts', 'dc_wpcf7_addon');
+
+function dc_wpcf7_addon($hook) {
+    $my_js_ver  = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'js/form.js' ));
+    wp_enqueue_script( 'form_js', plugins_url( 'js/form.js', __FILE__ ), array("jquery","contact-form-7"), $my_js_ver );
+}
