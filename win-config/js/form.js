9103a262f01ec5a6e97748438079821a63fab9da
Marco Michely <marco.michely@michely-web-engineering.de>
2019-05-06 09:45:30 +0200

    prevent "Cannot read property 'addEventListener' of null"

--- win-config/js/form.js
+++ win-config/js/form.js
@@ -1,8 +1,6 @@
 jQuery(document).ready(function() {
 
-    let wpcf7Elm = document.querySelector( '.wpcf7' );
-
-    wpcf7Elm.addEventListener( 'wpcf7submit', function( event ) {
+    document.querySelector( '.wpcf7' ).addEventListener( 'wpcf7submit', function( event ) {
         let action = jQuery("div#" + event.detail.id).find("form").attr("action");
         let $form = jQuery("<form id=\"dc_form\" action=\"" + action + "\" method=\"POST\"></form>");
         jQuery(event.detail.inputs).each(function(k,v) {
