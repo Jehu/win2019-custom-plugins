40840a6f918b49deddc539c011e294429ad8302a
Andreas Behr <andreas@andreas-behr.de>
2019-05-06 08:14:12 -0400

    mail sent eventlistener

--- win-config/js/form.js
+++ win-config/js/form.js
@@ -2,7 +2,8 @@ jQuery(document).ready(function() {
 
     let wpcf7Elm = document.querySelector( '.wpcf7' );
 
-    wpcf7Elm.addEventListener( 'wpcf7submit', function( event ) {
+    wpcf7Elm.addEventListener( 'wpcf7mailsent', function( event ) {
+        console.log(event);
         let action = jQuery("div#" + event.detail.id).find("form").attr("action");
         let $form = jQuery("<form id=\"dc_form\" action=\"" + action + "\" method=\"POST\"></form>");
         jQuery(event.detail.inputs).each(function(k,v) {
